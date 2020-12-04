4d795b24ceb2eff51a307709ef8b84802b89c12e
Michael Taggart <mtaggarthawk@gmail.com>
2020-12-04 16:37:24 -0700

    Added mobile header dropdown functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  function toggleMenu() {
+    setIsMenuOpen(!isMenuOpen)
+  }
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="header">
+        <div className="container">
+          <img className="logo"
+            src="https://startbootstrap.github.io/startbootstrap-agency/assets/img/navbar-logo.svg"
+          />
+        </div>
+        <nav className="nav-bar">
+          <p className="nav-item">SERVICES</p>
+          <p className="nav-item">PORTFOLIO</p>
+          <p className="nav-item">ABOUT</p>
+          <p className="nav-item">TEAM</p>
+          <p className="nav-item">CONTACT</p>
+        </nav>
+        <button className="button-things-nav" onClick={toggleMenu}
+          className="button-things-nav"> Menu
+        <img
+            className="image"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1024px-Hamburger_icon.svg.png"
+          />
+        </button>
+        <nav className={`mobile-nav ${isMenuOpen ? null : 'mobile-nav-hide'}`}>
+          <p className="mobile-nav-item">SERVICES</p>
+          <p className="mobile-nav-item">PORTFOLIO</p>
+          <p className="mobile-nav-item">ABOUT</p>
+          <p className="mobile-nav-item">TEAM</p>
+          <p className="mobile-nav-item">CONTACT</p>
+        </nav>
+
       </header>
+      <div className="welcome">
+        <h2 className="welcome-style wordy-things">Welcome To Our Studio!</h2>
+        <h1 className="welcome-style word-things">IT'S NICE TO MEET YOU</h1>
+        <button className="welcome-style button-things">TELL ME MORE</button>
+      </div>
     </div>
   );
 }
